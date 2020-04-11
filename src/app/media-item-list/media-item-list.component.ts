8b403a29b90a2608e51735dfc24745b0158ad504
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-11 20:40:32 +0200

    cofigured routing for based on mediums

--- src/app/media-item-list/media-item-list.component.ts
+++ src/app/media-item-list/media-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 import { MediaItemService } from '../services/media-item.service';
 
 
@@ -12,11 +13,20 @@ export class MediaItemListComponent implements OnInit {
   mediaItems: any[];
 
   constructor(
-    private mediaItemService: MediaItemService
+    private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute
     ){}
 
   ngOnInit(): void {
-    this.getMediaItems(this.medium);
+    this.activatedRoute.paramMap.subscribe(
+      paramMap => {
+        let medium = paramMap.get('medium');
+        if (medium.toLowerCase() === 'all') {
+          medium = '';
+        }
+        this.getMediaItems(medium);
+      }
+    )
   }
 
   onMediaItemDelete(mediaItem: any) {
